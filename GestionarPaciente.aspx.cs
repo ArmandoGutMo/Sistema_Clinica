be498f567d70bc36fd71f2382f72834bbd407ffc
ArmandoGutMo <argutmo819@gmail.com>
2025-08-04 17:21:27 -0500

    Agrega .gitignore y limpia archivos no deseados

---
+++ GestionarPaciente.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CapaEntidades;
+using CapaLogicaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class GestionarPaciente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                
+            }
+        }
+        [WebMethod]
+        public static List<Paciente> ListarPacientes()
+        {
+            List<Paciente> Lista = null;
+            try
+            {
+                Lista = PacienteLN.getInstance().ListarPacientes();
+            }
+            catch (Exception ex)
+            {
+                Lista = new List<Paciente>();
+            }
+            return Lista;
+        }
+
+        private Paciente GetEntity()
+        {
+            Paciente objPaciente = new Paciente();
+            objPaciente.IdPaciente = 0;
+            objPaciente.Nombres = txtNombres.Text;
+            objPaciente.ApPaterno = txtApPaterno.Text;
+            objPaciente.ApMaterno = txtApMaterno.Text;
+            objPaciente.Edad = Convert.ToInt32(txtEdad.Text);
+            objPaciente.Sexo = (ddlSexo.SelectedValue == "Femenino") ? "F" : "M";
+            objPaciente.NroDocumento = txtNroDocumento.Text;
+            objPaciente.Direccion = txtDireccion.Text;
+            objPaciente.Telefono = txtTelefono.Text;
+            objPaciente.Estado = true;
+
+            return objPaciente;
+
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Paciente objPaciente = GetEntity();
+           
+            bool response = PacienteLN.getInstance().RegistrarPaciente(objPaciente);
+            if (response)
+            {
+                Response.Write("<script>alert('REGISTRO CORRECTO.')</script>");
+
+            }
+            else
+            {
+                Response.Write("<script>alert('REGISTRO INCORRECTO.')</script>");
+            }
+        }
+
+
+    }
+}
---
+++ js/Paciente.js
@@ -0,0 +1,34 @@
+﻿function AddrowDT(data) {
+    tabla = $("#tbl_pacientes").Datatable();
+    for (var i = 0; i < datalength; i++) {
+        tabla.fnAddData([
+            data[i].IdPaciente,
+            data[i].Nombres,
+            (data[i].ApPaterno + "" + data[i].ApMaterno),
+            ((data[i].Sexo == 'M') ? 'Masculino' : 'Femenino'),
+            data[i].Edad,
+            data[i].Direccion,
+            data[i].Telefono,
+            ((data[i].Estado == true) ? "Activo" : "Inactivo")
+        ])
+    }
+}
+
+function sendDataAjax() {
+    $.ajax({
+        type: "POST",
+        url: "GestionarPaciente.aspx/ListarPacientes",
+        data: {},
+        contentType: 'application/json; charset=utf-8',
+
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status + " \n" + xhr.responseText, "\n" + thrownError);
+        },
+        success: function (data) {
+            console.log(data);
+            AddrowDT(data.d);
+        }
+    });
+}
+
+sendDataAjax;
